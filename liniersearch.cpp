4f34eb2892ba81eaa571894854b67b3a50a44f2d
aldys002 <igidiaaldys@gmail.com>
2024-04-04 14:49:29 +0700

    menambah fungsi for

--- liniersearch.cpp
+++ liniersearch.cpp
@@ -7,5 +7,20 @@ int i; // Index of array element
 
 void input()
 {
+    while(true)
+    {
+    cout << "enter the number of elements in the array : "'
+    cin >> n;
+    if ((n>0) && (n <= 20))
+         break;
+    else
+        cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n;
+    } 
     
+    // Accept array elements
+    cout << "\n-----------------------\n";
+    cout << "Enter array elemen \n";
+    coit << "-------------------------\n";
+    for (i=0; 1 < n; i++)
 }
+
