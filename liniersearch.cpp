76c34f2c9f732c218ec528993ecdb1cae9692f36
aldys002 <igidiaaldys@gmail.com>
2024-04-04 14:58:06 +0700

    add step 1

--- liniersearch.cpp
+++ liniersearch.cpp
@@ -38,7 +38,7 @@ void LinierSearch (){
     do
     {
         //Accept the number to be searched
-        
+        cout << "\nEnter the element you want to search: "; //step 1
     } while (/* condition */);
     
 }
