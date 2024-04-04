cb93864d7f385df88b257c4f015dc310c3140f29
aldys002 <igidiaaldys@gmail.com>
2024-04-04 16:01:27 +0700

    merubah semua codingan

--- liniersearch.cpp
+++ liniersearch.cpp
@@ -1,81 +1,74 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; //Array to be searched
-int n; // Number of elements in the array
-int i; // Index of array element
+int arr[20]; // Array to be searched
+int n; // number of elements in the array
+int i; // index of array element
 
-void input()
-{
-    while(true)
-    {
-       cout << "enter the number of elements in the array : ";
-       cin >> n;
-       if ((n > 0) && (n <= 20))
-            break;
-       else
-            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n ";
-     
-    }
-    
-    
-    // Accept array elements
-    cout << "\n----------------------\n";
-    cout << "Enter array elements \n";
-    cout << "--------------------\n";
-    for (i=0; 1 < n; i++)
-    {
-        cout << "<" << (i + 1) << ">";
-        cin >> arr[i];
-    }
+void input() {
+	while (true) {
+		cout << "Enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray shoult have minimum 1 and maximum 20 elements.\n\n";
+	}
+
+	// accept array elements
+	cout << "\n-----------------------\n";
+	cout << "Enter array elements \n";
+	cout << "---------------------\n"; 
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
 
-void LiniearSearch (){
-    char ch;
-    int ctr; //Number of comparisons
-    int item;
+void LinearSearch() {
+	char ch;
+	int ctr; // number of comparisons
+	int item;
 
-    do
-    {
-        //Accept the number to be searched
-        cout << "\nEnter the element you want to search: "; //step 1
-        cin >> item;
+	do {
+		//Accept the number to be searched
+		cout << "\nEnter the element you want to search: ";
+		cin >> item;
 
-        ctr = 0;
-        i = 0; //step 2
-        while (i < n) // step 3
-        {
-            ctr++;
-            if (arr[i] == item)
-            {
-                cout << "\n" << item << "found at position " << (i + 1) << endl; //step 6
-                break;
-            }   
-            i++; //step 4
-        }
+		ctr = 0;
+		i = 0; //step 2
+		while (i < n) // step 3
+		{
+			ctr++;
+			if (arr[i] == item) {
+				cout << "\n" << item << " fount at position " << (i + 1) << endl; // step 6
+				break;
+			}
+			i++;  // step 4
+		}
 
-        if (i == n) //step 5
-             cout << "\n" << item << "not found in the array\n";
-        cout << "\nNumber of comparisons: " << ctr << endl;
+		if (i == n) //step 5
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
 
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
 
-        cout << "\nContinue search (y/n): ";
-        cin >> ch;
-
-    } while ((ch == 'y') || (ch == 'Y'));
+	} while ((ch == 'y') || (ch == 'Y'));
 }
 
 void display()
 {
-    cout << "Array elements are: ";
-    for (i = 0; i < n; i++)
-       cout << arr[i] << "";
-    cout << endl;
+	cout << "Array element are: ";
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
 int main()
 {
-    input();
-    LiniearSearch();
-    display();
+	input();
+	LinearSearch();
+	display();
 }
